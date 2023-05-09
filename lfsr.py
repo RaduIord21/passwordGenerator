8e9450ac29e816af52a2d5f2c5334111443f8f77
RaduIord21 <nemus20@gmail.com>
2023-05-09 16:03:45 +0300

    Password_generator_imporved_with_hashes

--- lfsr.py
+++ lfsr.py
@@ -1,7 +1,7 @@
-from random import randint
-import sys
+from hashlib import sha256
 
-from time import time,sleep
+from time import time, sleep
+
 
 def xor(a, b):
     if a == b:
@@ -12,16 +12,26 @@ def xor(a, b):
 def lfsr():  # vor fi alese pozitiile 1, 2, 4, 5 si 8
     password = []
     seed = int(time() * 1000.0)
-    seed_digits = [int(d) for d in str(bin(seed))[2:]]
+    hashObj = sha256()
+    hashObj.update(bytes(str(seed), encoding="ascii"))
+    hashed_seed = hashObj.hexdigest()
+    hashed_seed_binary = [bin(ord(d)) for d in hashed_seed]
+    seed_digits = []
+    for each in hashed_seed_binary:
+        for bit in each[2:]:
+            seed_digits.append(bit)
     for each in range(16):
         password.append(seed_digits.pop())
-        seed_digits.insert(0, xor(xor(xor(xor(seed_digits[1], seed_digits[2]), seed_digits[4]), seed_digits[5]), seed_digits[8]))
+        seed_digits.insert(0, xor(xor(xor(xor(seed_digits[1], seed_digits[2]), seed_digits[4]), seed_digits[5]),
+                                  seed_digits[8]))
     return int("".join(str(x) for x in password), 2)
+
+
 def shuffle(lst):
     for each in range(len(lst)):
         sleep(0.1)
-        tmp = lst[lfsr() % len(lst)]
-        aux = lst[each]
-        lst[each] = tmp
-        tmp = aux
+        index_to_swap = lfsr() % len(lst)
+        aux = lst[index_to_swap]
+        lst[index_to_swap] = lst[each]
+        lst[each] = aux
     return lst
--- main.py
+++ main.py
@@ -7,12 +7,10 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 special_characters = ['!', '"', '#', '$', '%', '&', '\\', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=',
                       '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
-
 all_chars = letters + numbers + special_characters
 no_letters = len(letters)
 no_numbers = len(numbers)
 no_special_characters = len(special_characters)
-
 
 def main():
     password = []
@@ -26,32 +24,36 @@ def main():
         min_specials = int(input("Least amount of specials: "))
         max_specials = int(input("Most amount of specials: "))
         password_length = int(input("Password Length : "))
-        if min_specials + min_numbers + min_letters < password_length:
-            break
-        if max_specials + max_numbers + max_letters > password_length:
-            break
-        if min_letters < max_letters:
-            break
-        if min_numbers < max_numbers:
-            break
-        if min_specials < max_specials:
+        sum_minims = min_specials + min_numbers + min_letters
+        sum_max = max_specials + max_numbers + max_letters
+        if sum_minims <= password_length <= sum_max:
             break
         print("Invalid input !!!")
+        while True:
+            if min_letters < max_letters:
+                break
+            if min_numbers < max_numbers:
+                break
+            if min_specials < max_specials:
+                break
+            print("Invalid Input !!!")
     for each in range(min_letters + 1):
-        sleep(0.1)
+        sleep(0.01)
         password.append(letters[lfsr() % no_letters])
     for each in range(min_numbers):
-        sleep(0.1)
+        sleep(0.01)
         password.append(numbers[lfsr() % no_numbers])
     for each in range(min_specials):
-        sleep(0.1)
+        sleep(0.01)
         password.append(special_characters[lfsr() % no_special_characters])
     temp_len = len(password)
     for each in range(password_length - temp_len):
-        sleep(0.1)
+        sleep(0.01)
         password.append(all_chars[lfsr() % len(all_chars)])
+    shuffle(password)
     for each in password:
         result += each
     return result
+
 
 print(main())
